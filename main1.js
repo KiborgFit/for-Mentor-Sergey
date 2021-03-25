bb4f38e62f022f3108d3f3571fc7b8f30cd66e07
Kiborg <yury_kiborg@bk.ru>
2021-03-26 00:58:39 +0300

    Turning our block
    Turning our block on and off

---
+++ main1.js
@@ -0,0 +1,10 @@
+const moduleWindow = document.getElementById('block');
+
+const buttonEnter = document.getElementById('btn');
+
+const buttonClose = document.getElementById('button');
+
+buttonEnter.addEventListener('click', () => {moduleWindow.style.visibility = "visible"});
+
+btnEnter.addEventListener('click', () => {moduleWindow.style.animationPlayState = "string"});
+
